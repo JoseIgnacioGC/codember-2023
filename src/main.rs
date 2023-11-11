bc2e4a1c9c257e0fec93967b6094076b1b273753
Jose Leiva <email.joseleiva@gmail.com>
2023-11-11 03:31:56 -0300

    Move challenge 01 into a module

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,5 @@
-use indexmap::map::IndexMap;
-
-fn decode_message(msg: &str) -> String {
-    msg.split_whitespace()
-        .map(|word| word.to_lowercase())
-        .fold(IndexMap::<String, u16>::new(), |mut words, word| {
-            words
-                .entry(word)
-                .and_modify(|count| *count += 1)
-                .or_insert(1);
-            words
-        })
-        .iter()
-        .map(|(key, value)| format!("{}{}", key, value))
-        .collect::<String>()
-}
+mod challenge_01;
 
 fn main() {
-    let decoded = decode_message(MSG);
-    println!("decoded message: {}", decoded);
+    let _challenge_01 = challenge_01::decode_challenge_message();
 }
-
-#[test]
-fn decode_dog_cat() {
-    let decoded = decode_message("cat dog dog car Cat doG sun");
-    assert_eq!(decoded, "cat2dog3car1sun1");
-}
-
-#[test]
-fn decode_key_house() {
-    let decoded = decode_message("keys house HOUSE house keys");
-    assert_eq!(decoded, "keys2house3");
-}
-
-#[test]
-fn decode_cup_te() {
-    let decoded = decode_message("cup te a cup");
-    assert_eq!(decoded, "cup2te1a1");
-}
-
-#[test]
-fn decode_house() {
-    let decoded = decode_message("houses house housess");
-    assert_eq!(decoded, "houses1house1housess1");
-}
-
-const MSG: &str = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
