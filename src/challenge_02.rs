3cbf2c55e8330b8989f6093e6c73c64b7e1ef3d8
Jose Leiva <email.joseleiva@gmail.com>
2023-11-13 00:27:32 -0300

    Add challenge_02

---
+++ src/challenge_02.rs
@@ -0,0 +1,38 @@
+fn operate_with_symbols(symbols: &str) -> String {
+    let nums: String = symbols
+        .chars()
+        .fold((String::new(), 0), |(msg, num), char| {
+            let current_num: i32 = match char {
+                '#' => num + 1,
+                '@' => num - 1,
+                '*' => num * num,
+                '&' => num,
+                _ => panic!("symbol not supported"),
+            };
+            if current_num == num {
+                let msg = format!("{}{}", msg, current_num);
+                (msg, num)
+            } else {
+                (msg, current_num)
+            }
+        })
+        .0;
+    nums
+}
+
+pub fn operate_challenge_message() -> String {
+    operate_with_symbols(OPERATION)
+}
+
+#[test]
+fn operation_1() {
+    let operation = operate_with_symbols("##*&");
+    assert_eq!(operation, "4");
+}
+
+#[test]
+fn operation_2() {
+    let operation = operate_with_symbols("&##&*&@&");
+    assert_eq!(operation, "0243");
+}
+const OPERATION: &str = "&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&";
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod challenge_01;
+mod challenge_02;
 
 fn main() {
     let _challenge_01 = challenge_01::decode_challenge_message();
+    let challenge_02 = challenge_02::operate_challenge_message();
+    println!("challenge_02: {}", challenge_02);
 }
